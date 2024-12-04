40c6526903b2370d3384450778a1fdf2f4f64c4e
wtomaz808 <wtomaz808@gmail.com>
2024-12-04 11:28:23 -1000

    againagin

--- src/ContosoSuitesWebAPI/Services/IDatabaseService.cs
+++ src/ContosoSuitesWebAPI/Services/IDatabaseService.cs
@@ -1,12 +1,102 @@
-using global::ContosoSuitesWebAPI.Entities;
+using System.Runtime.CompilerServices;
+using Microsoft.Data.SqlClient;
+using ContosoSuitesWebAPI.Entities;
 
 namespace ContosoSuitesWebAPI.Services;
 
-public interface IDatabaseService
+/// <summary>
+/// The database service for querying the Contoso Suites database.
+/// </summary>
+public class DatabaseService : IDatabaseService
 {
-    Task<IEnumerable<Hotel>> GetHotels();
-    Task<IEnumerable<Booking>> GetBookingsForHotel(int hotelId);
-    Task<IEnumerable<Booking>> GetBookingsByHotelAndMinimumDate(int hotelId, DateTime dt);
-    Task<IEnumerable<Booking>> GetBookingsWithMultipleHotelRooms();
-    Task<IEnumerable<Booking>> GetBookingsMissingHotelRooms();
+    /// <summary>
+    /// Get all hotels from the database.
+    /// </summary>
+    public async Task<IEnumerable<Hotel>> GetHotels()
+    {
+        var sql = "SELECT HotelID, HotelName, City, Country FROM dbo.Hotel";
+        using var conn = new SqlConnection(
+            connectionString: Environment.GetEnvironmentVariable("SQLAZURECONNSTR_ContosoSuites")!
+        );
+        conn.Open();
+        using var cmd = new SqlCommand(sql, conn);
+        using var reader = await cmd.ExecuteReaderAsync();
+        var hotels = new List<Hotel>();
+        while (await reader.ReadAsync())
+        {
+            hotels.Add(new Hotel
+            {
+                HotelID = reader.GetInt32(0),
+                HotelName = reader.GetString(1),
+                City = reader.GetString(2),
+                Country = reader.GetString(3)
+            });
+        }
+        conn.Close();
+
+        return hotels;
+    }
+
+    /// <summary>
+    /// Get a specific hotel from the database.
+    /// </summary>
+    public async Task<IEnumerable<Booking>> GetBookingsForHotel(int hotelId)
+    {
+        var sql = "SELECT BookingID, CustomerID, HotelID, StayBeginDate, StayEndDate, NumberOfGuests FROM dbo.Booking WHERE HotelID = @HotelID";
+        using var conn = new SqlConnection(
+            connectionString: Environment.GetEnvironmentVariable("SQLAZURECONNSTR_ContosoSuites")!
+        );
+        conn.Open();
+        using var cmd = new SqlCommand(sql, conn);
+        cmd.Parameters.AddWithValue("@HotelID", hotelId);
+        using var reader = await cmd.ExecuteReaderAsync();
+        var bookings = new List<Booking>();
+        while (await reader.ReadAsync())
+        {
+            bookings.Add(new Booking
+            {
+                BookingID = reader.GetInt32(0),
+                CustomerID = reader.GetInt32(1),
+                HotelID = reader.GetInt32(2),
+                StayBeginDate = reader.GetDateTime(3),
+                StayEndDate = reader.GetDateTime(4),
+                NumberOfGuests = reader.GetInt32(5)
+            });
+        }
+        conn.Close();
+
+        return bookings;
+    }
+
+    /// <summary>
+    /// Get bookings for a specific hotel that are after a specified date.
+    /// </summary>
+    public async Task<IEnumerable<Booking>> GetBookingsByHotelAndMinimumDate(int hotelId, DateTime dt)
+    {
+        var sql = "SELECT BookingID, CustomerID, HotelID, StayBeginDate, StayEndDate, NumberOfGuests FROM dbo.Booking WHERE HotelID = @HotelID AND StayBeginDate >= @StayBeginDate";
+        using var conn = new SqlConnection(
+            connectionString: Environment.GetEnvironmentVariable("SQLAZURECONNSTR_ContosoSuites")!
+        );
+        conn.Open();
+        using var cmd = new SqlCommand(sql, conn);
+        cmd.Parameters.AddWithValue("@HotelID", hotelId);
+        cmd.Parameters.AddWithValue("@StayBeginDate", dt);
+        using var reader = await cmd.ExecuteReaderAsync();
+        var bookings = new List<Booking>();
+        while (await reader.ReadAsync())
+        {
+            bookings.Add(new Booking
+            {
+                BookingID = reader.GetInt32(0),
+                CustomerID = reader.GetInt32(1),
+                HotelID = reader.GetInt32(2),
+                StayBeginDate = reader.GetDateTime(3),
+                StayEndDate = reader.GetDateTime(4),
+                NumberOfGuests = reader.GetInt32(5)
+            });
+        }
+        conn.Close();
+
+        return bookings;
+    }
 }
